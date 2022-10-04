4a83a9d25c74cd024bad65aa98df27be88a350d5
_Branavan_ <10540209+BranavanSelvasingham@users.noreply.github.com>
2022-10-04 10:52:12 -0400

    Delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export PACKAGE_DIRS=$CURRENT_DIR/packages:$
-
-export PACKAGE_DIRS='/Users/branavan/NimbusPOS/Meteor App/packages'
-
-export MONGO_URL=mongodb://maestro-uat:49eHDAwgrfvV@ds019063.mlab.com:19063/maestro-uat
